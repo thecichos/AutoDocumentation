108bb0724eb94f8d28b88a16034f66f7d6fc9ad6
jonas <thecichos@gmail.com>
2026-01-06 17:15:23 +0100

    Add Response attribute to AutoDocumentation library for method-level metadata definition

---
+++ AutoDocumentation/Attributes/Response.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AutoDocumentation\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
+class Response
+{
+	public function __construct(
+		public int $statusCode,
+		public string $description,
+		public ?string $type = null
+	)
+	{
+	}
+}
