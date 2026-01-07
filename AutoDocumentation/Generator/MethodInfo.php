e00464c6659b5e7278b2ad6373dc0baa64990140
jonas <thecichos@gmail.com>
2026-01-07 15:57:02 +0100

    Docs: added documentation attributes to some more of the codebase

--- AutoDocumentation/Generator/MethodInfo.php
+++ AutoDocumentation/Generator/MethodInfo.php
@@ -2,17 +2,34 @@
 
 namespace AutoDocumentation\Generator;
 
+use AutoDocumentation\Attributes\Documentable;
+use AutoDocumentation\Attributes\Param;
+
+#[Documentable(
+	description: 'Represents metadata about a class method for documentation purposes',
+	slug: 'method-info',
+	group: 'Generator'
+)]
 readonly class MethodInfo
 {
 	public function __construct(
+		#[Param(description: 'The name of the method', example: 'getUserById')]
 		public string $name,
+		#[Param(description: 'Human-readable description of what the method does')]
 		public string $description,
+		#[Param(description: 'Array of MethodParamInfo objects describing the method parameters')]
 		public array $parameters,
+		#[Param(description: 'The return type as a string, or null if void/untyped', required: false)]
 		public ?string $returnType,
+		#[Param(description: 'Description of what the method returns', required: false)]
 		public ?string $returnDescription,
+		#[Param(description: 'Example usage or return value of the method', required: false)]
 		public ?string $example,
+		#[Param(description: 'Whether the method is marked as deprecated')]
 		public bool $deprecated,
+		#[Param(description: 'Whether the method is static')]
 		public bool $isStatic,
+		#[Param(description: 'Visibility level of the method', required: false)]
 		public Accessibility $accessibility = Accessibility::Public,
 	) {}
 }
--- AutoDocumentation/Generator/MethodParamInfo.php
+++ AutoDocumentation/Generator/MethodParamInfo.php
@@ -2,13 +2,27 @@
 
 namespace AutoDocumentation\Generator;
 
+use AutoDocumentation\Attributes\Documentable;
+use AutoDocumentation\Attributes\Param;
+
+#[Documentable(
+	description: 'Represents metadata about a method parameter for documentation purposes',
+	slug: 'method-param-info',
+	group: 'Generator'
+)]
+
 readonly class MethodParamInfo
 {
 	public function __construct(
+		#[Param(description: 'The name of the parameter', example: 'userId')]
 		public string $name,
+		#[Param(description: 'The type of the parameter as a string', example: 'int')]
 		public string $type,
+		#[Param(description: 'Whether the parameter can be null')]
 		public bool $nullable,
+		#[Param(description: 'Whether the parameter has a default value')]
 		public bool $hasDefault,
+		#[Param(description: 'The default value of the parameter, or null if none')]
 		public mixed $default,
 	) {}
 }
